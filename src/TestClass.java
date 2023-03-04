2094a57acc9b10e0c0ee24261c3676834868f517
anhnuoi <anhlbvbnd26@gmail.com>
2023-03-04 21:18:50 +0700

    First Commit in Ngocanhnubranch

---
+++ src/TestClass.java
@@ -0,0 +1,9 @@
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Ngoc anh nu, this is new branch!!!");
+        boolean isAilen=false;
+        if(isAilen==true)
+            System.out.println("This is Ailen!");
+        else System.out.println("THis is not Ailen!");
+    }
+}
